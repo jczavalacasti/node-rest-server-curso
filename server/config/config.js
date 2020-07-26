498ceb3c287413a68500211d4a4cf432c068d7df
julio zavala <zavalajc.20@gmail.com>
2020-07-26 14:19:26 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =========================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://cafe-user:admin123@cluster0.ksss7.mongodb.net/cafe?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
